f2b0dc7f02d28ecedfee7671f4fc906405f7f407
nithishkrishna <nithishkrishna2002@gmail.com>
2025-12-11 12:16:19 -0500

    DateTime format fix and preprocessing code add

--- database.py
+++ database.py
@@ -23,9 +23,21 @@ class Neo4jConnection:
     
     def execute_query(self, query, parameters=None):
         """Execute a Cypher query and return results"""
+        from neo4j.time import DateTime, Date
+
+        def convert_neo4j_types(obj):
+            """Convert Neo4j datetime types to strings"""
+            if isinstance(obj, (DateTime, Date)):
+                return obj.isoformat()
+            elif isinstance(obj, dict):
+                return {k: convert_neo4j_types(v) for k, v in obj.items()}
+            elif isinstance(obj, list):
+                return [convert_neo4j_types(item) for item in obj]
+            return obj
+
         with self.get_session() as session:
             result = session.run(query, parameters or {})
-            return [record.data() for record in result]
+            return [convert_neo4j_types(record.data()) for record in result]
     
     def execute_write(self, query, parameters=None):
         """Execute a write query"""
